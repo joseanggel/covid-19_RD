714614f659d7b3bf7398117dfe50944dcef5af24
joseangel <joseangelmf27@gmail.com>
2020-04-13 18:23:34 -0400

    Add all countries

--- app/src/main/java/com/example/covid_19_rd/MainActivity.java
+++ app/src/main/java/com/example/covid_19_rd/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.covid_19_rd;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,30 +15,23 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-
         ListView listView = findViewById(R.id.lvCountry);
-
-        final String[] systemList = getResources().getStringArray(R.array.country);
+        final String[] countryList = getResources().getStringArray(R.array.country);
 
         ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, android.R.id.text1, systemList
+                this, android.R.layout.simple_list_item_1, android.R.id.text1, countryList
         );
 
         listView.setAdapter(mAdapter);
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-//
                 String value = String.valueOf(position);
                 Intent i = new Intent(MainActivity.this, Web.class);
                 i.putExtra("key",value);
                 startActivity(i);
-
-
             }
         });
     }
--- app/src/main/java/com/example/covid_19_rd/Web.java
+++ app/src/main/java/com/example/covid_19_rd/Web.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+
+
 public class Web extends AppCompatActivity {
 
     @Override
@@ -14,99 +16,118 @@ public class Web extends AppCompatActivity {
 
         WebView myWebView = findViewById(R.id.webview);
 
+        final String[] countryList = getResources().getStringArray(R.array.countryEN);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String value = extras.getString("key");
 
+
             assert value != null;
+
             if(value.equals("0")){
 
                 myWebView.loadUrl("https://www.worldometers.info/coronavirus/");
 
             }
-            if(value.equals("1")){
 
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/us/");
+            for(int i = 1; i <= systemList.length; i++){
+                String value2 = String.valueOf(i);
+                if(value.equals(value2)){
+                    myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/" + countryList[i]);
+                }
 
-            }
 
-            if(value.equals("2")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/italy/");
-
-            }
-
-            if(value.equals("3")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/spain/");
 
             }
-
-            if(value.equals("4")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/france/");
-
-            }
-
-            if(value.equals("5")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/china/");
-
-            }
-
-            if(value.equals("6")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/iran/");
-
-            }
-
-            if(value.equals("7")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/dominican-republic/");
-
-            }
-
-            if(value.equals("8")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/germany/");
-
-            }
-
-            if(value.equals("9")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/uk/");
-
-            }
-
-            if(value.equals("10")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/belgium/");
-
-            }
-
-            if(value.equals("11")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/mexico/");
-
-            }
-
-            if(value.equals("12")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/turkey/");
-
-            }
-
-            if(value.equals("13")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/germany/");
-
-            }
-
-            if(value.equals("13")){
-
-                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/netherlands/");
-
-            }
+//            if(value.equals("0")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/");
+//
+//            }
+//            if(value.equals("1")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/afghanistan");
+//
+//            }
+//
+//            if(value.equals("2")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/italy/");
+//
+//            }
+//
+//            if(value.equals("3")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/spain/");
+//
+//            }
+//
+//            if(value.equals("4")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/france/");
+//
+//            }
+//
+//            if(value.equals("5")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/china/");
+//
+//            }
+//
+//            if(value.equals("6")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/iran/");
+//
+//            }
+//
+//            if(value.equals("7")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/dominican-republic/");
+//
+//            }
+//
+//            if(value.equals("8")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/germany/");
+//
+//            }
+//
+//            if(value.equals("9")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/uk/");
+//
+//            }
+//
+//            if(value.equals("10")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/belgium/");
+//
+//            }
+//
+//            if(value.equals("11")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/mexico/");
+//
+//            }
+//
+//            if(value.equals("12")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/turkey/");
+//
+//            }
+//
+//            if(value.equals("13")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/germany/");
+//
+//            }
+//
+//            if(value.equals("13")){
+//
+//                myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/netherlands/");
+//
+//            }
 
 
 
